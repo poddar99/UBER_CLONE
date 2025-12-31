ad9439e1a0d6b686870b9f25e8eb81f7a4106ada
Soumya Deep Poddar <soumyazzz99@gmail.com>
2025-12-31 16:44:13 +0530

    captain done

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,17 @@ const cookieParser = require('cookie-parser');
 
 const connectToDb=require('./db/db');
 const userRoutes=require('./routes/user.routes');
+const captainRoutes=require('./routes/captain.routes');
+
+
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
+app.use(cookieParser()) ;
+app.use('/captains',captainRoutes);
+
 
 app.get('/',(req, res)=>{
     res.send('hello');
